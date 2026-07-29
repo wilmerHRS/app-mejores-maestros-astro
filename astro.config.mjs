314d7c935e70cd7ad2dbbc1c871fc8e2e38d07a8
Wilmer Reluz <wilmer.r2004@gmail.com>
2026-07-29 13:29:38 -0500

    feat: install and configure Tailwind CSS v4 using Vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  output: 'server'
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
